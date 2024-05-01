f78c4b71659a2f0c2280a2f8e98741379d5d8842
Pablo_P <pizzanux@gmail.com>
2024-05-01 09:50:55 -0300

    Fuente escala de Re

---
+++ D_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de RE")
+winsound.Beep(293,1000)
+winsound.Beep(329,1000)
+winsound.Beep(369,1000)
+winsound.Beep(391,1000)
+winsound.Beep(440,1000)
+winsound.Beep(493,1000)
+winsound.Beep(554,1000)
+winsound.Beep(587,1000)
