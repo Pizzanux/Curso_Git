2d1cb227d6d2626c7ad99d0870a8c097d47962a1
Pablo_P <pizzanux@gmail.com>
2024-05-01 09:39:12 -0300

    Fuente escala de Do

---
+++ C_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de DO")
+winsound.Beep(261,1000)
+winsound.Beep(293,1000)
+winsound.Beep(329,1000)
+winsound.Beep(349,1000)
+winsound.Beep(391,1000)
+winsound.Beep(440,1000)
+winsound.Beep(493,1000)
+winsound.Beep(523,1000)
