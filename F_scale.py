6a1404223d924911d12cd3f31d55caaeeea7968d
Pablo_P <pizzanux@gmail.com>
2024-05-03 17:55:37 -0300

    Fuente escala de Fa

---
+++ F_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de Fa")
+winsound.Beep(349,1000)
+winsound.Beep(391,1000)
+winsound.Beep(440,1000)
+winsound.Beep(466,1000)
+winsound.Beep(523,1000)
+winsound.Beep(587,1000)
+winsound.Beep(659,1000)
+winsound.Beep(698,1000)
