d019eca41e15fa5ec39a1370aeee2776d21d5ecf
Pablo_P <pizzanux@gmail.com>
2024-05-10 07:44:20 -0300

    Fuente escala de Sol

---
+++ G_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de G")
+winsound.Beep(391,1000)
+winsound.Beep(440,1000)
+winsound.Beep(493,1000)
+winsound.Beep(523,1000)
+winsound.Beep(587,1000)
+winsound.Beep(659,1000)
+winsound.Beep(739,1000)
+winsound.Beep(783,1000)
