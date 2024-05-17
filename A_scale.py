fab0b240e8d036efed73d7aefaf02f5337e75677
Pizzanux <pizzanux@gmail.com>
2024-05-17 18:17:23 -0300

    Fuente escala de La

---
+++ A_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de A")
+winsound.Beep(440,1000)
+winsound.Beep(493,1000)
+winsound.Beep(554,1000)
+winsound.Beep(587,1000)
+winsound.Beep(659,1000)
+winsound.Beep(739,1000)
+winsound.Beep(830,1000)
+winsound.Beep(880,1000)
