e2ac9904d525e37b3e358114d972aab7f725b21c
Pizzanux <pizzanux@gmail.com>
2024-06-10 08:12:13 -0300

    Escala de F sostenido

---
+++ F#_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de Fa#")
+winsound.Beep(369,1000)
+winsound.Beep(415,1000)
+winsound.Beep(466,1000)
+winsound.Beep(493,1000)
+winsound.Beep(554,1000)
+winsound.Beep(622,1000)
+winsound.Beep(698,1000)
+winsound.Beep(739,1000)
