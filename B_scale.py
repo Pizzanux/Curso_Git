d394d88016537f50ade5ffd13fdb2e3126199ad9
Pizzanux <pizzanux@gmail.com>
2024-06-12 08:25:37 -0300

    Fuente escala de Si

---
+++ B_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de Si")
+winsound.Beep(493,1000)
+winsound.Beep(554,1000)
+winsound.Beep(622,1000)
+winsound.Beep(659,1000)
+winsound.Beep(739,1000)
+winsound.Beep(830,1000)
+winsound.Beep(932,1000)
+winsound.Beep(987,1000)
