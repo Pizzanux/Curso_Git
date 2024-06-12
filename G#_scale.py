4531d2083b74a417a1040a1860995fd405eb57e1
Pizzanux <pizzanux@gmail.com>
2024-06-12 08:16:59 -0300

    Fuente escala de Sol#

---
+++ G#_scale.py
@@ -0,0 +1,11 @@
+import winsound
+
+print("Escala de G#")
+winsound.Beep(415,1000)
+winsound.Beep(466,1000)
+winsound.Beep(523,1000)
+winsound.Beep(554,1000)
+winsound.Beep(622,1000)
+winsound.Beep(698,1000)
+winsound.Beep(783,1000)
+winsound.Beep(830,1000)
